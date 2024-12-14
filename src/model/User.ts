51a0ec8441160108abacee225a93e1ba7b3aef00
pritom.dutta <pritom.dutta@rahitechnologies.com>
2024-12-15 00:50:36 +0530

    created model and schemas for user and messages

---
+++ src/model/User.ts
@@ -0,0 +1,76 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import mongoose, { Schema, Document } from "mongoose";
+// Document - use for type safety - used with typescript, for JS not required
+
+// Message interface
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+// Message schema
+const MessageSchema = new Schema<Message>({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+// User interface
+export interface User extends Document {
+  userName: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: Message[];
+}
+
+// Use schema
+const UserSchema = new Schema<User>({
+  userName: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [
+      // use for email validation - you can get this regex from 'regexr' website or chatgpt
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+      "Please use a valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify code expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+  messages: [MessageSchema],
+});
